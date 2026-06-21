6090b861fbfd372f7feb77ba6e189f2db9b5d073
RAMUSSS7 <ramiazrou10@gmail.com>
2026-06-21 13:13:03 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// تشغيل الملفات الثابتة من المجلد الرئيسي
 app.use(express.static(path.join(__dirname, '/')));
 
 const rooms = {};
@@ -78,7 +79,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
